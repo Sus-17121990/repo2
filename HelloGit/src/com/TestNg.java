724101744c18df648ea102c36c8449b36b023d5e
Susanta Mondal <susanta.mondal06@gmail.com>
2018-02-07 01:05:48 +0530

    modified TestNg

--- HelloGit/src/com/TestNg.java
+++ HelloGit/src/com/TestNg.java
@@ -1,11 +1,13 @@
 package com;
 
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 public class TestNg {
+	@Parameters({"name"})
 	@Test
-	public void Test1(){
-		System.out.println("Susanta");
+	public void Test1(String name){
+		System.out.println(name);
 	}
 
 }

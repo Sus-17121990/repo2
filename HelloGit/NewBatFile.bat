8ad39ad146ef38587836bf0895de4bc275e6c922
Susanta Mondal <susanta.mondal06@gmail.com>
2018-02-07 23:14:40 +0530

    new files

--- HelloGit/NewBatFile.bat
+++
@@ -1,5 +0,0 @@
-set projectLocation=C:\Users\nso7691\git\repo2\HelloGit
-cd %projectLocation%
-set classpath=%projectLocation%\bin;%projectLocation%\lib\*
-java org.testng.TestNG %projectLocation%\testng.xml
-pause
